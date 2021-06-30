c4fa2a903f4f81976ed2d75e32c2b2e87ae7ad9f
sksat <sksat@sksat.net>
2021-07-01 02:01:03 +0900

    add: Paper commit update script

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# check latest commit
+git clone --depth 1 https://github.com/PaperMC/Paper.git
+cd Paper
+PAPER_COMMIT_LATEST=$(git rev-parse HEAD)
+cd ..
+rm -rf Paper
+
+# diff commit
+source .env
+diff <(echo $PAPER_COMMIT) <(echo $PAPER_COMMIT_LATEST)
+if [ $? -eq 0 ]; then
+	echo "no update"
+	exit 0
+fi
+
+# update
+echo "PAPER_COMMIT=${PAPER_COMMIT_LATEST}" > .env
