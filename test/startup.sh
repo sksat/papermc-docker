9585dd838475f3f4754656cf8f40d5ce79041a11
sksat <sksat@sksat.net>
2021-07-03 06:47:55 +0900

    テストの追加 (#6)
    * add: startup test
    
    * add: IMG_TAG to .env
    
    * update: use IMG_TAG in startup test
    
    * fix: login to ghcr.io in test
    
    * fix: image path
    
    * fix: CI trigger
    
    * test: Java11(this check must be fail)
    
    * fix: split test docker-compose.yml
    
    * add: cache docker layers, install mcstatus
    
    * fix: service down check, exit if mcstatus offline
    
    * fix: add cache config to build-push-action
    
    * Revert "test: Java11(this check must be fail)"
    
    This reverts commit 3bac3d9d9d31854a9e06749d2a57261b9d676162.
    
    * fix: docker build cache
    
    * add: check Minecraft version in test

---
+++ test/startup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+cd `dirname $0`
+pwd
+
+source ../.env
+if [ $# -eq 1 ]; then
+	IMG_TAG=$1
+fi
+
+mkdir -p data
+echo "eula=true" > data/eula.txt
+
+echo "image tag: ${IMG_TAG}"
+env IMG_TAG="${IMG_TAG}" docker-compose up -d
+sleep 5
+docker-compose ps
+
+if [ ! -z "$(docker-compose ps | grep 'Exit 1')" ]; then
+	echo "error: service is down"
+	exit 1
+fi
+
+echo "wait for start..."
+sleep 60
+docker-compose logs
+
+MCSTATUS_JSON=$(mcstatus localhost json)
+echo "${MCSTATUS_JSON}"
+
+MCSTATUS_ONLINE=$(echo ${MCSTATUS_JSON} | jq .online)
+MCSTATUS_VERSION=$(echo ${MCSTATUS_JSON} | jq .version)
+
+if [ "${MCSTATUS_ONLINE}" != 'true' ]; then
+	echo "Minecraft server is down"
+	exit 1
+fi
+
+if [ "${MCSTATUS_VERSION}" != "\"${MINECRAFT_VERSION}\"" ]; then
+	echo "Minecraft version mismatch: ${MCSTATUS_VERSION}"
+	exit 1
+fi
+
+env IMG_TAG="${IMG_TAG}" docker-compose down
+rm -rf data
+exit 0
