0c310d14cd78c6d88b5d9d1da2a1d50b35744a34
sksat <sksat@sksat.net>
2021-07-23 08:03:26 +0900

    update

--- update.sh
+++ update.sh
@@ -4,11 +4,8 @@
 git clone --depth 1 https://github.com/PaperMC/Paper.git
 cd Paper
 
-tmp_ver=$(cat gradle.properties | grep 'version =')
-tmp_mcver=$(cat gradle.properties | grep 'mcVersion')
-
-MINECRAFT_VERSION_LATEST=$(echo "${tmp_mcver}" | sed -e "s/mcVersion = //")
-PAPER_VERSION_LATEST=$(echo "${tmp_ver}" | sed -e "s/version = //")
+MINECRAFT_VERSION_LATEST=$(cat gradle.properties | grep 'mcVersion =' | sed -e 's/mcVersion = //')
+PAPER_VERSION_LATEST=$(cat gradle.properties | grep 'version =' | sed -e 's/version = //')
 PAPER_COMMIT_LATEST=$(git rev-parse HEAD)
 
 cd ..
@@ -20,10 +17,16 @@ echo "IMG_TAG=${IMG_TAG}" >> .env_latest
 echo "MINECRAFT_VERSION='${MINECRAFT_VERSION_LATEST}'" >> .env_latest
 echo "PAPER_VERSION='${PAPER_VERSION_LATEST}'" >> .env_latest
 echo "PAPER_COMMIT=${PAPER_COMMIT_LATEST}" >> .env_latest
+
 diff .env .env_latest
 if [ $? -eq 0 ]; then
 	echo "no update"
 	exit 0
+fi
+
+diff <(echo $MINECRAFT_VERSION) <(echo $MINECRAFT_VERSION_LATEST)
+if [ $? -ne 0 ]; then
+	echo "Minecraft version update!!!!!"
 fi
 
 # update
