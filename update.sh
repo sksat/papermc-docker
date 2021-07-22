3d12bf537590bc8e72d2d3a75766a1c570d9f91c
sksat <sksat@sksat.net>
2021-07-23 06:57:33 +0900

    add: MINECRAFT_VERSION

--- update.sh
+++ update.sh
@@ -3,19 +3,27 @@
 # check latest commit
 git clone --depth 1 https://github.com/PaperMC/Paper.git
 cd Paper
+
+tmp_ver=$(cat gradle.properties | grep 'version =')
+tmp_mcver=$(cat gradle.properties | grep 'mcVersion')
+MINECRAFT_VERSION_LATEST=${tmp_mcver:12}
+PAPER_VERSION_LATEST=${tmp_ver:10}
 PAPER_COMMIT_LATEST=$(git rev-parse HEAD)
+
 cd ..
 rm -rf Paper
 
-# diff commit
+# check diff
 source .env
-diff <(echo $PAPER_COMMIT) <(echo $PAPER_COMMIT_LATEST)
+echo "IMG_TAG=${IMG_TAG}" >> .env_latest
+echo "MINECRAFT_VERSION='${MINECRAFT_VERSION_LATEST}'" >> .env_latest
+echo "PAPER_VERSION='${PAPER_VERSION_LATEST}'" >> .env_latest
+echo "PAPER_COMMIT=${PAPER_COMMIT_LATEST}" >> .env_latest
+diff .env .env_latest
 if [ $? -eq 0 ]; then
 	echo "no update"
 	exit 0
 fi
 
 # update
-sed -e '/PAPER_COMMIT=/d' .env > .env_new
-echo "PAPER_COMMIT=${PAPER_COMMIT_LATEST}" >> .env_new
-mv .env_new .env
+mv .env_latest .env
