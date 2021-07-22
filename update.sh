a6d44683d39b85337a9029d0d04617b18e2517a8
sksat <sksat@sksat.net>
2021-07-23 07:24:53 +0900

    fix: PAPER_VERSION style

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ tmp_ver=$(cat gradle.properties | grep 'version =')
 tmp_mcver=$(cat gradle.properties | grep 'mcVersion')
 
 MINECRAFT_VERSION_LATEST=$(echo "${tmp_mcver}" | sed -e "s/mcVersion = //")
-PAPER_VERSION_LATEST=$(echo "${tmp_ver}" | sed -e "s/version = //" | sed -e "s/${MINECRAFT_VERSION_LATEST}-//")
+PAPER_VERSION_LATEST=$(echo "${tmp_ver}" | sed -e "s/version = //")
 PAPER_COMMIT_LATEST=$(git rev-parse HEAD)
 
 cd ..
