8b0c0a41c542c69298e6390786ae71b4625b275d
sksat <sksat@sksat.net>
2021-09-21 10:25:29 +0900

    add: start_period check

--- health.sh
+++ health.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
+cd `dirname $0`
 
 SERVER="localhost"
 
 if [ $# -ne 0 ]; then
   SERVER="$1"
+fi
+
+if [ ! -e /tmp/healthcheck-start ]; then
+	date '+%s' > /tmp/healthcheck-start
+else
+	start_time=$(cat /tmp/healthcheck-start)
+	elapsed=$(expr `date +%s` - $start_time)
+	if [ $elapsed -lt 300 ]; then
+		exit 0
+	fi
 fi
 
 timeout 10 mc-monitor status --host ${SERVER} --port 25565
