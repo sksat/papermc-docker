eb6cd0917dcc0b04b3b253f2580a7266765493db
sksat <sksat@sksat.net>
2021-09-25 07:26:37 +0900

    add: digest to openjdk image

--- utils/java.sh
+++ utils/java.sh
@@ -2,9 +2,12 @@
 
 # Official OpenJDK images
 OPENJDK=(
-  "openjdk:16-slim amd64,arm64/v8"
-  "openjdk:16-slim-buster amd64,arm64/v8"
-  "openjdk:16-oraclelinux8 amd64,arm64/v8"
+  "openjdk:16-slim amd64            sha256:d3cb56aa5360a4cb7a3dd1b38e40c155ac1064ff9b944da8c1c03456df3496e1"
+  #"openjdk:16-slim arm64/v8         sha256:d7e6afd5155486baacd0895276cc74d534babc17153776730509df4550db450d"
+  "openjdk:16-slim-buster amd64     sha256:98425688c0bdacdee571eb6bb189d6267eba622d3755f4713b7ef1de6fa16841"
+  #"openjdk:16-slim-buster arm64/v8  sha256:eb2c3e29cd8bead0443372f887d58ebf3ae163f674caeda4251cd5f37b93f4ec"
+  "openjdk:16-oraclelinux8 amd64    sha256:367103dddb6ed4cf9c0e69963f2cf22dbfee114934db8516338b84a0864f70dc"
+  #"openjdk:16-oraclelinux8 arm64/v8 sha256:af04c50184d0227ece48c85e5535d622a9f4ed649d5f18e0f18f58c006b934c8"
 )
 
 # Non-Official AdoptOpenJDK images(but AdoptOpenJDK maintained)
@@ -33,9 +36,10 @@ function openjdk_imgs(){
 		oj=(${openjdk[@]})
 		img="${oj[0]}"
 		arch="${oj[1]}"
+		digest="${oj[2]}"
 
 		tag=$(cut -d':' -f 2 <<<${img})
-		echo "\"$img\""
+		echo "\"${img}@${digest}\""
 	done
 }
 
