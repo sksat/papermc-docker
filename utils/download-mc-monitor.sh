f883e5466a7a63044f6f675739553cbe7215bf9f
sksat <sksat@sksat.net>
2021-12-19 01:06:27 +0900

    fix: download arm64 version mc-monitor in arm64 image build

--- utils/download-mc-monitor.sh
+++ utils/download-mc-monitor.sh
@@ -3,9 +3,15 @@
 # datasource=github-releases depName=itzg/mc-monitor
 MC_MONITOR_VERSION="0.10.6"
 
+MC_MONITOR_ARCH="amd64"
+case $(uname -m) in
+	x86_64) MC_MONITOR_ARCH="amd64" ;;
+	aarch64) MC_MONITOR_ARCH="arm64" ;;
+esac
+
 mkdir mc-monitor
 cd mc-monitor
-wget "https://github.com/itzg/mc-monitor/releases/download/${MC_MONITOR_VERSION}/mc-monitor_${MC_MONITOR_VERSION}_linux_amd64.tar.gz" -O mc-monitor.tar.gz
+wget "https://github.com/itzg/mc-monitor/releases/download/${MC_MONITOR_VERSION}/mc-monitor_${MC_MONITOR_VERSION}_linux_${MC_MONITOR_ARCH}.tar.gz" -O mc-monitor.tar.gz
 ls
 tar -xzvf mc-monitor.tar.gz
 ls mc-monitor
